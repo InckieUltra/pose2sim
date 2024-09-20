f2efd9912ea40833e225a106f3df90b8f67988fa
Inckie <35866488+InckieUltra@users.noreply.github.com>
2024-09-20 16:05:06 +0800

    test

---
+++ Pose2Sim/Demo_SinglePerson/test.py
@@ -0,0 +1,9 @@
+from Pose2Sim import Pose2Sim
+#Pose2Sim.calibration()
+#Pose2Sim.poseEstimation()
+#Pose2Sim.synchronization()
+#Pose2Sim.personAssociation()
+#Pose2Sim.triangulation()
+#Pose2Sim.filtering()
+#Pose2Sim.markerAugmentation()
+Pose2Sim.kinematics()
